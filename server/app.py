5baf9635f66ad1e51471d5778f024f7bbf6a7466
jayson <jayson.ambrose.87@gmail.com>
2023-05-15 11:55:50 -0600

    initial commit

---
+++ server/app.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from flask import request, make_response, session
+from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+
+from config import app, db, api
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
