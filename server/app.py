838b30d9c90a31e8776cec5b349b61656f005026
jayson <jayson.ambrose.87@gmail.com>
2023-05-16 21:13:47 -0600

    cards can be added to db

--- server/app.py
+++ server/app.py
@@ -7,5 +7,17 @@ from models import Card, KeywordMap, ColorMap
 
 from config import app, db, api
 
+class Cards(Resource):
+    
+    def get(self):
+        card_list = []
+        for card in Card.query.all():
+            card_list.append(card.to_dict(rules=('-keyword_map.card_id', '-keyword_map.id',
+                                                 '-color_map.card_id', '-color_map.id')))
+
+        return make_response(card_list, 200)
+    
+api.add_resource(Cards, '/cards')
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -15,11 +15,41 @@ class Card(db.Model, SerializerMixin):
     img_url = db.Column(db.String)
     text = db.Column(db.Text)
 
+    keyword_map = db.relationship('KeywordMap', backref='card', cascade='all, delete-orphan')
+    color_map = db.relationship('ColorMap', backref='card', cascade='all, delete-orphan')
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    def buildKeyWordMap(self, n_cancel, n_collect, n_destroy, n_discarded, n_draw, n_expansion, n_remove, n_renews):
+
+        new_map = KeywordMap(cancel=n_cancel, collect=n_collect, destroy=n_destroy,
+                             discarded=n_discarded, draw=n_draw, expansion=n_expansion,
+                             remove=n_remove, renews=n_renews, card=self)
+        
+        try:
+            db.session.add(new_map)
+            db.session.commit()
+
+        except ValueError:
+            print('failed to add KeywordMap')
+
+
+    def buildColorMap(self, n_red, n_blue, n_green, n_purple):
+
+        new_map = ColorMap(red=n_red, blue=n_blue, green=n_green, purple=n_purple, card=self)
+        
+        try:
+            db.session.add(new_map)
+            db.session.commit()
+
+        except ValueError:
+            print('failed to add ColorMap')
+
 class KeywordMap(db.Model, SerializerMixin):
     __tablename__ = 'keyword_maps'
+
+    serialize_rules = ('-card',)
 
     id = db.Column(db.Integer, primary_key=True)
     cancel = db.Column(db.Boolean)
@@ -31,17 +61,23 @@ class KeywordMap(db.Model, SerializerMixin):
     remove = db.Column(db.Boolean)
     renews = db.Column(db.Boolean)
 
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.id'))
+
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
 class ColorMap (db.Model, SerializerMixin):
     __tablename__ = 'color_maps'
 
+    serialize_rules = ('-card',)
+
     id = db.Column(db.Integer, primary_key=True)
     red = db.Column(db.Boolean)
     blue = db.Column(db.Boolean)
     green = db.Column(db.Boolean)
     purple = db.Column(db.Boolean)
+
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.id'))
 
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
